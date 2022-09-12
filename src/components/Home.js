37f77ceadbd66cca8d115f7ee0fcde2858df4699
Pete Fowler <fowler.pete@gmail.com>
2022-09-12 16:53:34 -0600

    Display flex column, show another button

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,30 +11,20 @@ function Home() {
 
     fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&sort=random&diet=vegan`)
     .then(res => res.json())
-    .then(data => setRecipe(data.results[0]))
-    .then(fetch2());
-  }
-
-  function fetch2() {
-    const key = '7c9862ec65e5475e978e284fa042e7df';
-
-    fetch(`https://api.spoonacular.com/recipes/${recipe.id}/card?apiKey=${key}`)
-    .then(res => res.json())
-    .then(data => setCard(data.url));
+    .then(data => fetch(`https://api.spoonacular.com/recipes/${data.results[0].id}/card?apiKey=${key}`)
+      .then(res => res.json())
+      .then(data => setCard(data.url)));
   }
 
   useEffect(() => {
     fetch1();
   }, []);
 
-  // useEffect(() => {
-  //   fetch2();
-  // }, [recipe])
 
   return <div id='home'>
     <h1>Recipe of the Day</h1>
     <button type='button' onClick={fetch1}>Show Me Another</button>
-    <img src={card} alt={recipe.title}></img>
+    <img src={card} alt='Recipe card'></img>
   </div>
 }
 
