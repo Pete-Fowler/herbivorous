c910e7f687e3c514040c6203d0f199358cbf347a
Pete Fowler <fowler.pete@gmail.com>
2022-09-12 16:32:16 -0600

    Display recipe card

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,29 +3,37 @@ import React, { useEffect, useState } from 'react';
 import Recipe from './Recipe';
 
 function Home() {
-const [ recipe, setRecipe ] = useState({id: '', title: '', image: ''});
-const [ card, setCard ] = useState('');
+  const [ recipe, setRecipe ] = useState({id: '', title: '', image: ''});
+  const [ card, setCard ] = useState('');
 
-useEffect(() => {
-  const key = '7c9862ec65e5475e978e284fa042e7df';
+  function fetch1() {
+    const key = '7c9862ec65e5475e978e284fa042e7df';
 
-  fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&sort=random&diet=vegan`)
-  .then(res => res.json())
-  .then(data => {
-    setRecipe(data.results[0]);
-  });
-}, []);
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&sort=random&diet=vegan`)
+    .then(res => res.json())
+    .then(data => setRecipe(data.results[0]))
+    .then(fetch2());
+  }
 
-useEffect(() => {
-  const key = '7c9862ec65e5475e978e284fa042e7df';
+  function fetch2() {
+    const key = '7c9862ec65e5475e978e284fa042e7df';
 
-  fetch(`https://api.spoonacular.com/recipes/${recipe.id}/card?apiKey=${key}`)
-  .then(res => res.json())
-  .then(data => setCard(data.url));
-}, [recipe])
+    fetch(`https://api.spoonacular.com/recipes/${recipe.id}/card?apiKey=${key}`)
+    .then(res => res.json())
+    .then(data => setCard(data.url));
+  }
+
+  useEffect(() => {
+    fetch1();
+  }, []);
+
+  // useEffect(() => {
+  //   fetch2();
+  // }, [recipe])
 
   return <div id='home'>
-    {/* <Recipe id={recipe.id} title={recipe.title} image={recipe.image}/> */}
+    <h1>Recipe of the Day</h1>
+    <button type='button' onClick={fetch1}>Show Me Another</button>
     <img src={card} alt={recipe.title}></img>
   </div>
 }
