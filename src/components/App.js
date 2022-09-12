70cb23212887a6c48ad34636122e167ee8eead07
Pete Fowler <fowler.pete@gmail.com>
2022-09-12 17:39:32 -0600

    Draft top level functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import '../styles/App.css';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Nav from './Nav';
 import Home from './Home';
 import Search from './Search';
@@ -7,11 +7,34 @@ import Submit from './Submit';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
+  
+  const randomRecipe = useRef({}) 
+  const [ randomCard, setRandomCard ] = useState('');
+  const key = '7c9862ec65e5475e978e284fa042e7df';
+
+  function getRandomRecipe() {
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&sort=random&diet=vegan`)
+    .then(res => res.json())
+    .then(data => {
+      randomRecipe.current = data.results[0].id;
+    });
+  }
+
+  useEffect(() => {
+    getRandomRecipe();
+  }, []);
+
+  useEffect(() => {
+    fetch(`https://api.spoonacular.com/recipes/${randomRecipe.current}/card?apiKey=${key}`)
+    .then(res => res.json())
+    .then(data => setRandomCard(data.url));
+  }, [randomRecipe])
+  
   return (
     <div id='app'>
       <Nav />
       <Routes>
-        <Route exact path='/' element={<Home />} />
+        <Route exact path='/' element={<Home imageUrl={randomCard} />} />
         <Route path='/search' element={<Search />} />
         <Route path='/submit' element={<Submit />} />
       </Routes>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,12 @@
 import '../styles/Home.css';
-import React, { useEffect, useState } from 'react';
-import Recipe from './Recipe';
 
-function Home() {
-  const [ recipe, setRecipe ] = useState({id: '', title: '', image: ''});
-  const [ card, setCard ] = useState('');
-
-  function fetch1() {
-    const key = '7c9862ec65e5475e978e284fa042e7df';
-
-    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&sort=random&diet=vegan`)
-    .then(res => res.json())
-    .then(data => fetch(`https://api.spoonacular.com/recipes/${data.results[0].id}/card?apiKey=${key}`)
-      .then(res => res.json())
-      .then(data => setCard(data.url)));
-  }
-
-  useEffect(() => {
-    fetch1();
-  }, []);
-
+function Home({ randomCard, newCard}) {
+  
 
   return <div id='home'>
     <h1>Recipe of the Day</h1>
-    <button type='button' onClick={fetch1}>Show Me Another</button>
-    <img src={card} alt='Recipe card'></img>
+    <button type='button' onClick={newCard}>Show Me Another</button>
+    <img src={randomCard} alt='Recipe card'></img>
   </div>
 }
 
