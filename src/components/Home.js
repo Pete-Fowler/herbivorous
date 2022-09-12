d147fee406362e0420eaa905ed6ab90ed6e2c00f
Pete Fowler <fowler.pete@gmail.com>
2022-09-12 15:35:38 -0600

    Draft display card

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import Recipe from './Recipe';
 
 function Home() {
 const [ recipe, setRecipe ] = useState({id: '', title: '', image: ''});
+const [ card, setCard ] = useState('');
 
 useEffect(() => {
   const key = '7c9862ec65e5475e978e284fa042e7df';
@@ -12,12 +13,20 @@ useEffect(() => {
   .then(res => res.json())
   .then(data => {
     setRecipe(data.results[0]);
-    console.log(data.results[0]);
-  }); 
+  });
 }, []);
 
+useEffect(() => {
+  const key = '7c9862ec65e5475e978e284fa042e7df';
+
+  fetch(`https://api.spoonacular.com/recipes/${recipe.id}/card?apiKey=${key}`)
+  .then(res => res.json())
+  .then(data => setCard(data.url));
+}, [recipe])
+
   return <div id='home'>
-    <Recipe id={recipe.id} title={recipe.title} image={recipe.image}/>
+    {/* <Recipe id={recipe.id} title={recipe.title} image={recipe.image}/> */}
+    <img src={card} alt={recipe.title}></img>
   </div>
 }
 
