c96229b3ce3cfad279c84533fcb58432f2312ff4
Pete Fowler <fowler.pete@gmail.com>
2022-09-12 15:15:55 -0600

    Fetch random recipe listing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,23 @@
-import React from 'react';
 import '../styles/Home.css';
+import React, { useEffect, useState } from 'react';
+import Recipe from './Recipe';
 
 function Home() {
+const [ recipe, setRecipe ] = useState({id: '', title: '', image: ''});
+
+useEffect(() => {
+  const key = '7c9862ec65e5475e978e284fa042e7df';
+
+  fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&sort=random&diet=vegan`)
+  .then(res => res.json())
+  .then(data => {
+    setRecipe(data.results[0]);
+    console.log(data.results[0]);
+  }); 
+}, []);
 
   return <div id='home'>
-    Home
+    <Recipe id={recipe.id} title={recipe.title} image={recipe.image}/>
   </div>
 }
 
