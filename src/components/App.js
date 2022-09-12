1e89e2cdf1bbca8f48f37d84d431f9a1ea13ae28
Pete Fowler <fowler.pete@gmail.com>
2022-09-12 13:26:48 -0600

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>App 
+      {/* <Switch>
+        <Route>
+
+        </Route>
+
+        <Route>
+
+        </Route>
+        <Route>
+
+        </Route>
+      </Switch> */}
+    </div>
+  );
+}
+
+export default App;
