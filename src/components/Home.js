b2a4124d55a307144131471ff08cd6a6c0f96ada
Pete Fowler <fowler.pete@gmail.com>
2022-09-13 16:32:03 -0600

    Add liked button to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,21 @@
 import '../styles/Home.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 function Home({ imageUrl, anotherRandomCard }) {
+  const [ liked, setLiked ] = useState(false);
   
+  function like(e) {
+    setLiked(liked => !liked);
+
+  }
+
   return <div id='home'>
     <h1>Recipe of the Day</h1>
     <button type='button' onClick={anotherRandomCard}>Show Me Another</button>
-    <img src={imageUrl} alt='Recipe card'></img>
+    <div id='image-wrapper'>
+      <div id='likeBtn' onClick={like}>{liked ? '❤️' : '🤍'}</div>
+      <img src={imageUrl} alt='Recipe card'></img>
+    </div>
   </div>
 }
 
